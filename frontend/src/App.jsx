fced717ea9796a4f9c21c6fe0dc9167dd1997515
suveerbabu <suveerprasad100@gmail.com>
2025-05-24 22:40:21 +0530

    merge conflicts resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Start from './pages/Start';
-<<<<<<< HEAD
-=======
-import Home from './pages/Home';
 import ProfileSetup from './pages/ProfileSetup';
->>>>>>> d8e5a25341853bf460d37020d248cd66b0ebfc6f
 import Chat from './pages/Chat';
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
